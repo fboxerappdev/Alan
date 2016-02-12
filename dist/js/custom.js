0aebdf4b2738ebbef80feb730a36d15f816e4f5c
gurpal <gurpal713@gmail.com>
2016-02-12 18:53:30 +0530

    home page done

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -1,13 +1,14 @@
 
 
 		$(document).ready(function(){
-
-
-		
 			$('.reviews-slider').bxSlider({
 				auto: true,
 				autoControls: true
 			});
 
-
+			var winwith = $(document).width();
+			if(winwith<768){
+				$(".vt-center").find("br").remove();
+				$(".social-icons").insertAfter(".footer-block img");
+			}
 		});
